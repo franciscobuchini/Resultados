b9dc3dab7a1c655d86cc038f60463dd42fd977c0
Francisco Buchini <fran@camarai.es>
2026-04-23 19:34:08 -0300

    readme

--- src/components/ReadmeViewer.tsx
+++ src/components/ReadmeViewer.tsx
@@ -6,16 +6,15 @@ export default function ReadmeViewer() {
   return (
     <div className="max-w-4xl mx-auto mt-20 mb-32 px-6 text-white">
       Cosas a resolver:
-    <br/> - Penales: hay una columna goles_penales que no se está usando porque estan en la columna de comentarios
-    <br/> - Goles: no hay goles
-    <br/> - Tournament_winner_id: completar si ese Torneo requiere un campeón.
-    <br/> - Tabla de usuarios (inicio de sesión)
-    <br/> - Completar tabla de equipos.
-    <br/> - Normalizacion de nombres de los equipos. (Resolver problema de Central Córdoba, etc) 
-    <br/> - Zonas: zona A zona B
-    <br/> - Hay partidos unicos entre campeonatos
-    <br/> - FALTA Copa de la superliga 2019.
-    
+    <br/> - Penales: hay una columna goles_penales que no se está usando porque estan en la columna de comentarios (F)
+    <br/> - Goles: no hay goles (J)
+    <br/> - Tournament_winner_id: completar si ese Torneo requiere un campeón. (J)
+    <br/> - Tabla de usuarios (inicio de sesión) (F)
+    <br/> - Normalizacion de nombres de los equipos. (Resolver problema de Central Córdoba, etc) (F)
+    <br/> - Zonas: zona A zona B (F)
+    <br/> - Hay partidos unicos entre campeonatos (J)
+    <br/> - FALTA Copa de la superliga 2019. (J)
+    <br/> - Cargar todos los campeonatos que faltan (J)
     </div>
   )
 }
