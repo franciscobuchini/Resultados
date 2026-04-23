348e71f75843d4af601ffe5819a3b934dd669dee
Francisco Buchini <fran@camarai.es>
2026-04-23 18:11:49 -0300

    fix de sincronización

--- src/components/ReadmeViewer.tsx
+++ src/components/ReadmeViewer.tsx
@@ -1,31 +1,18 @@
-import { useEffect, useState } from 'react'
+
 
 export default function ReadmeViewer() {
-  const [content, setContent] = useState('')
 
-  useEffect(() => {
-    // En Vite podemos importar archivos locales usando fetch o import?raw
-    // Para simplificar y que sea dinámico, lo leemos de la raíz
-    fetch('/README.md')
-      .then(res => res.text())
-      .then(text => setContent(text))
-      .catch(() => setContent('No se pudo cargar el README.md'))
-  }, [])
-
-  if (!content) return null
 
   return (
-    <div className="max-w-4xl mx-auto mt-20 mb-32 px-6">
-      <div className="border-t border-zinc-800 pt-8">
-        <h3 className="text-zinc-500 text-[10px] font-mono uppercase mb-6 tracking-widest">
-          Documentación / Notas del Proyecto
-        </h3>
-        <div className="bg-zinc-950/50 border border-zinc-900 rounded-xl p-8 shadow-2xl">
-          <pre className="text-zinc-400 text-xs font-mono whitespace-pre-wrap leading-relaxed">
-            {content}
-          </pre>
-        </div>
-      </div>
+    <div className="max-w-4xl mx-auto mt-20 mb-32 px-6 text-white">
+      Cosas a resolver:
+    <br/> - Penales: hay una columna goles_penales que no se está usando porque estan en la columna de comentarios
+    <br/> - Goles: no hay goles
+    <br/> - Tournament_winner_id: completar si ese Torneo requiere un campeón.
+    <br/> - Tabla de usuarios (inicio de sesión)
+    <br/> - Completar tabla de equipos.
+    <br/> - Normalizacion de nombres de los equipos. (Resolver problema de Central Córdoba, etc) 
+    <br/> - sincronizador ligado al UTC
     </div>
   )
 }
